d28a8119dfc0d4312b92c7e8be87e8eeb6742361
alifnew8531 <139584880+alifnew8531@users.noreply.github.com>
2026-05-04 01:07:36 +0600

    Learned Variable

---
+++ src/Variable.java
@@ -0,0 +1,16 @@
+public class Variable {
+    public static void main(String[] args) {
+        int age;
+        int year;
+        int quantity;
+        double price = 20.87;
+        char grade = 'A';
+        boolean isPassed = true;
+
+        if(isPassed){
+            System.out.println("Alif passed in 5th Semester");
+        }else{
+            System.out.println("Alif not passed in 5th Semester");
+        }
+    }
+}
